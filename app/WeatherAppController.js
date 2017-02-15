ec75cc2b10a1610e3e0ae51cf874c3f58936b026
Ke Wang <Ke Wang>
2017-02-14 21:04:18 -0800

    main feature completed

--- app/WeatherAppController.js
+++ app/WeatherAppController.js
@@ -5,16 +5,35 @@
         .module('WeatherApp')
         .controller('WeatherAppController', WeatherAppController);
 
-    WeatherAppController.$inject = ['WeatherAppFactory'];
+    WeatherAppController.$inject = ['WeatherAppFactory', 'toastr'];
 
     /* @ngInject */
-    function WeatherAppController(WeatherAppFactory) {
+    function WeatherAppController(WeatherAppFactory, toastr) {
         var vm = this;
-
-        activate();
-
-        function activate() {
-
+        vm.search = search;
+        vm.cities = [];
+        vm.defaultCity = "tokyo";
+        //toastr
+        function search(city) {
+          WeatherAppFactory.getWeather(city).then(
+            function(response){
+              vm.cityWeather = response.data;
+              toastr.success('We have found weather for ' + city);
+              console.log(vm.cityWeather);
+              vm.cities.push({
+                'name': vm.cityWeather.name,
+                'time': new Date()
+              });
+              vm.city = "";
+            })
+            .catch(function(error){
+              if(error.data) {
+                toastr.error('There was a big problem: ' + error.data.message);
+              } else {
+                toastr.info('no data in my data!');
+              }
+              console.log(error);
+            });
         }
     }
 })();
